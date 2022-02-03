de6150c1b4254e29731997b9e60c9e4f516872c2
Davi <88855441+davi12345452@users.noreply.github.com>
2022-02-03 16:34:55 -0300

    Delete desafio107.py

--- desafio107.py
+++
@@ -1,7 +0,0 @@
-# Programa que define uma função para uso posterior como módulo
-
-
-def modulo(num):
-    print(f"A metade de R${num} é R${num / 2}.")
-    print(f"O dobro de R${num} é R${num * 2}.")
-    print(f"Aumentando 10% de R${num} temos R${num * 1.1}.")
