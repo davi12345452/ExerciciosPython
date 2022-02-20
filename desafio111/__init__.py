de2b8191a1c2dfb29a48fc33b2005b9487ba6e86
Davi <88855441+davi12345452@users.noreply.github.com>
2022-02-19 21:39:27 -0300

    Add files via upload

---
+++ desafio111/__init__.py
@@ -0,0 +1,11 @@
+# Testes
+
+
+from utilidadecev import moeda as md
+
+p = float(input("Digite o valor: "))
+md.resumo(p, 80, 40)
+md.resumo(p, 100, 20)
+md.resumo(p, 30, 60)
+md.resumo(p, 60, 50)
+md.resumo(p, 10, 30)
